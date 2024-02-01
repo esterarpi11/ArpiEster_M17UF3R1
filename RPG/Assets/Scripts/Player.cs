d7408a0daa42de89f3625d9c0433bc5820fd517a
esterarpi11 <132432358+esterarpi11@users.noreply.github.com>
2024-02-01 16:45:03 +0100

    Input system

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameInputs;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class MainPlayer : MonoBehaviour
 {
     protected Character player;
-    protected int coins;
+    protected Inputs _inputs;
+    protected Rigidbody _rigidBody;
+    protected Animator _animator;
+
+    private void Awake()
+    {
+        _inputs = new Inputs();
+        _inputs.MainPlayer.Enable();
+        _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        _inputs.MainPlayer.Movement.performed += Move_Performed;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Move_Performed(InputAction.CallbackContext obj)
+    {
+        _animator.SetBool("running", true);
+        _rigidBody.velocity = obj.ReadValue<Vector2>() * player.speed;
     }
 }
