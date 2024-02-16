a574fe222985e3ce244496886a948c2df6c64ad8
AdrianRoBo <adrian.rodriguez.7e6@itb.cat>
2024-02-16 17:44:39 +0100

    Input cambiado a behaviour

---
+++ RPG/Assets/Scripts/InputController.cs
@@ -0,0 +1,62 @@
+using GameInputs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController Instance;
+    protected Inputs _inputs;
+
+    public static event Action Crouch = delegate { };
+    public static event Action Walk = delegate { };
+    public static event Action Run = delegate { };
+
+    Vector2 currentMovement;
+
+    private void Awake()
+    {
+        _inputs = new Inputs();
+        _inputs.MainPlayer.Movement.performed += ctx =>
+        {
+            //currentMovement = ctx.ReadValue<Vector2>();
+            Walk.Invoke();
+        }; 
+        _inputs.MainPlayer.Run.performed += ctx =>
+        {            
+            Run.Invoke();
+        };
+        _inputs.MainPlayer.Crouch.performed += ctx =>
+        {           
+            Crouch.Invoke();
+        };
+
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else Destroy(this.gameObject);
+    }
+    private void OnEnable()
+    {
+        _inputs.MainPlayer.Enable();
+    }
+    private void OnDisable()
+    {
+        _inputs.MainPlayer.Disable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    
+}
